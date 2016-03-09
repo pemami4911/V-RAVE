7847e89daf9471c67bb0a9f6c23303f389156d58
aemery <agemery2@gmail.com>
2016-03-09 15:56:05 -0500

    SpawnController: add old SpawnController

---
+++ Assets/AISpawnController/AISpawnController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AISpawnController : MonoBehaviour {
+
+	public AISpawnModel spawnModel;
+
+	// Use this for initialization
+	void Start () {
+
+		spawnModel = new AISpawnModel ();
+		GameObject AICar = Resources.Load ("AICar", typeof(GameObject)) as GameObject;
+
+		foreach (Vector3 coordinate in spawnModel.coordinates) {
+			Instantiate(AICar, coordinate, transform.rotation);
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/AISpawnController/AISpawnModel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class AISpawnModel {
+
+	public List<Vector3> coordinates {get; set;}
+
+	public AISpawnModel() {
+		coordinates = new List<Vector3> ();
+		Vector3 aiCar0 = new Vector3 (0, 0, 0); 
+
+
+		coordinates.Add(aiCar0);
+	}
+}
