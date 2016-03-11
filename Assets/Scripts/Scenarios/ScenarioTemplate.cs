c4cfb80e7a2f6333b4380f3602ac4e1bbbca27ee
Patrick Emami <patrickemami@gmail.com>
2016-03-11 11:41:47 -0500

    SCENARIOS: Added state machine lib, trigger script
    Added the MonsterLove state machine lib we'll be using
    for our scenarios. I made some minor mods to it, namely
    modded it to support the "OnTriggerEnter" callback as a
    trigger for state transitions and a "ChangeState" hook
    that takes in a trigger id as an arg
    
    Added a template for how a scenario script will look - these
    scripts will be attached to an empty game object in your scene
    
    Added a generic "trigger" script that can be attached to any
    gameobject with a triggerable collider, to be used to initiate
    a state transition (e.g. it calls your scenarios `ChangeState(id)`
    method)

---
+++ Assets/Scripts/Scenarios/ScenarioTemplate.cs
@@ -0,0 +1,81 @@
+﻿	using UnityEngine;
+	using System.Collections;
+	using MonsterLove.StateMachine;
+
+	namespace VRAVE 
+	{
+	public class ScenarioTemplate : StateBehaviour {
+
+		// Each scenario will want a reference to the user car
+		public GameObject UserCar; 
+
+		// You'll need to specify all of the states for your scenario here
+		// I recommend drawing an FSM to give you an idea about how state transitions
+		// will happen in your scenario
+		public enum States 
+		{
+			AIDriving,
+			Obstacle,
+			Exit
+		}
+
+		// Utilize the Vehicle Factory here to create any vehicles that need to be made at the beginning of the scenario
+		// It is probably a good idea to instantiate all of the vehicles that will be used at this time, and just disable
+		// the ones that shouldn't be visible yet
+		void Awake()
+		{
+			Initialize<States> ();
+			//Instantiate (UserCar, new Vector3 (21.11f, 0.14f, 14.4f), Quaternion.identity);
+			UserCar.SetActive (false);
+
+			// Set the initial state here
+			ChangeState (States.AIDriving);
+		}
+
+		// Extend abstract method "ChangeState(uint id)
+		//
+		// This is used for reacting to "OnTriggerEnter" events, called by WaypointTrigger scripts
+		public override void ChangeState (uint id)
+		{
+			switch (id) 
+			{
+			case 0: 
+				ChangeState (States.Obstacle);
+				break;
+			}
+		}
+
+		// State customizations
+		//
+		// Use the "StateName_Enter", "StateName_Update", etc naming convention, 
+		// Since the StateEngine uses reflection to execute the correct method 
+		// in the correct state
+		void AIDriving_Enter()
+		{
+			UserCar.SetActive (true);
+		}
+
+		void AIDriving_Update()
+		{
+			Debug.Log ("Driving...");
+		}
+
+		void Obstacle_Enter()
+		{
+			// Stick it 30 units in front of the car
+			Vector3 obstaclePosition = UserCar.transform.position + new Vector3 (0f, 0f, 30f);
+			GameObject cubeObstacle = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			
+				Debug.Log ("Making cube");
+				Instantiate (cubeObstacle, obstaclePosition, Quaternion.identity);
+
+			ChangeState (States.Exit);
+		}
+
+		void Exit_Enter()
+		{
+			Debug.Log ("Finished scenario test!");
+			
+		}
+	}
+	}
---
+++ Assets/Scripts/Scenarios/Trigger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using MonsterLove.StateMachine;
+
+namespace VRAVE
+{
+public class Trigger : MonoBehaviour {
+
+		// Number the triggers in your scenario, and use this to change the 
+		// state appropriately in your state machine
+		[SerializeField] private uint triggerID; 
+		// The scenario which contains this trigger
+		[SerializeField] private StateBehaviour scenario; 
+		// Tag of GameObject colliding with this collider 
+		[SerializeField] private string tag; 
+
+		// If this script is attached to a GameObject with a collider
+		// that has "isTrigger" selected, this callback will fire upon collision. 
+		void OnTriggerEnter(Collider other)
+		{
+			if (!other.CompareTag(tag))
+			{
+				return;
+			}
+				
+			scenario.ChangeState (triggerID);
+		}
+}
+}
