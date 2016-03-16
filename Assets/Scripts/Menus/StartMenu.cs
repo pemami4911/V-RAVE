b0645c197956259c0d3111ca440a1ddfc817745c
Patrick Emami <patrickemami@gmail.com>
2016-03-16 15:39:16 -0400

    START_MENU: Added placeholder start menu

---
+++ Assets/Scripts/Menus/StartMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class StartMenu : MonoBehaviour {
+
+	void Update() {
+		if (Input.GetKeyDown (KeyCode.Return)) {
+			SceneManager.LoadScene ("Lobby");
+		}
+	}
+
+}
