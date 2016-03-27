a8405b056790646b39137362b48efffbf3a3dbee
bsperling <bryce.h.sperling@gmail.com>
2016-03-27 17:21:54 -0400

    LANE_PASSING: Added rudimentary lane passing functionality.

--- Assets/Scripts/Scenarios/LanePassingScenario/LanePassingScenario.cs
+++ Assets/Scripts/Scenarios/LanePassingScenario/LanePassingScenario.cs
@@ -9,8 +9,14 @@ namespace VRAVE
     public class LanePassingScenario : StateBehaviour
     {
 
-        [SerializeField] private GameObject UserCar;
-        [SerializeField] private GameObject AIVehicle;
+        [SerializeField]
+        private GameObject UserCar;
+        [SerializeField]
+        private GameObject AIVehicle;
+        [SerializeField]
+        private WaypointCircuit passingTrack;
+        [SerializeField]
+        private WaypointCircuit initialTrack;
 
         private SpawnController manufacturer;
         private HUDController hudController;
@@ -22,8 +28,8 @@ namespace VRAVE
         private CarController AIVehicleCarController;
 
         private bool userMode = true;
+        private int circuitProgressNum = 0;
 
-        private float intersectionStopThreshold = 5f;
 
         public enum States
         {
@@ -32,6 +38,7 @@ namespace VRAVE
             Following,
             PassingInstruction,
             Passing,
+            PassingWait,
             Mode_Switch
         }
 
@@ -40,7 +47,7 @@ namespace VRAVE
             Initialize<States>();
 
             userCarController = UserCar.GetComponent<CarController>();
-            userCarController.MaxSpeed = 30f;
+            userCarController.MaxSpeed = 15f;
             userCarAI = UserCar.GetComponent<CarAIControl>();
             userCarAI.enabled = false;
             UserCar.GetComponent<CarUserControl>().enabled = false;
@@ -48,7 +55,7 @@ namespace VRAVE
 
 
             AIVehicleCarController = AIVehicle.GetComponent<CarController>();
-            AIVehicleCarController.MaxSpeed = 30f;
+            AIVehicleCarController.MaxSpeed = 15f;
             AIVehicleAI = AIVehicle.GetComponent<CarAIControl>();
             AIVehicleAI.enabled = false;
             (AIVehicle.GetComponent("Halo") as Behaviour).enabled = false;
@@ -83,6 +90,17 @@ namespace VRAVE
                         //ChangeState(States.WarnAndRestart);
                     }
                     break;
+                case 3: //Beginning of passing track
+                    Debug.Log("Inside Case 3");
+
+
+                    break;
+                case 4: //End of passing track
+                    Debug.Log("Inside Case 4");
+                    ChangeState(States.PassingInstruction);
+
+
+                    break;
             }
         }
 
@@ -95,10 +113,10 @@ namespace VRAVE
             Debug.Log("Enter: InitState");
             //UseCar and AI Vehicles Created
             //Display Scenario Information on HUD
-            
+
             //Start when right paddle is pressed
             //ChangeState(States.FollowingInstruction);
-            
+
         }
 
         // Wait for the user to press OK
@@ -117,7 +135,18 @@ namespace VRAVE
         public void FollowingInstruction_Enter()
         {
             Debug.Log("Enter: FollowingInstruction");
-            (UserCar.GetComponent<CarUserControl>() as CarUserControl).enabled = true;      
+
+            if(userMode)
+            {
+                //(UserCar.GetComponent<CarUserControl>() as CarUserControl).enabled = true;
+                ChangeState(States.PassingInstruction);
+            }
+            else
+            {
+                //NEEDS TO BE FIXED FOR FOLLOWING HANDLING
+                ChangeState(States.PassingInstruction);
+            }
+            
         }
 
         public void FollowingInstruction_Update()
@@ -136,21 +165,22 @@ namespace VRAVE
         public void Following_Enter()
         {
             Debug.Log("Entered: Following");
-            WaypointCircuit wc = GameObject.Find("Figure8_North_3-22").GetComponent<WaypointCircuit>();
-            AIVehicleAI.Circuit = wc;
+            //GameObject[] go = GameObject.FindGameObjectsWithTag("Path");
+            //initialTrack = GameObject.Find("Figure8_North_3-22").GetComponent<WaypointCircuit>();
+            AIVehicleAI.Circuit = initialTrack;
             AIVehicleAI.enabled = true;
             AIVehicleAI.IsCircuit = true;
             (AIVehicle.GetComponent("Halo") as Behaviour).enabled = true;
 
             if (userMode)
             {
-                (UserCar.GetComponent<CarUserControl>() as CarUserControl).enabled = true;       
+                (UserCar.GetComponent<CarUserControl>() as CarUserControl).enabled = true;
                 userCarAI.enabled = false;
             }
             else
             {
                 (UserCar.GetComponent<CarUserControl>() as CarUserControl).enabled = false;
-                userCarAI.Circuit = wc;
+                userCarAI.Circuit = initialTrack;
                 userCarAI.enabled = false;
             }
 
@@ -159,7 +189,9 @@ namespace VRAVE
         public void Following_Update()
         {
             Debug.Log("Update: Following");
-            WaypointCircuit wc = GameObject.Find("Figure8_North_3-22").GetComponent<WaypointCircuit>();
+
+            //TODO: REMOVE!!!
+            //WaypointCircuit wc = GameObject.Find("Figure8_North_3-22").GetComponent<WaypointCircuit>();
             Dictionary<int, VRAVE.VRAVEObstacle> vo;
             bool scanned = userCarSensors.Scan(out vo);
             Debug.Log(scanned);
@@ -172,12 +204,24 @@ namespace VRAVE
                     if (vo[7].Distance <= 10f)
                     {
                         Debug.Log("Sensor 7 in length");
-                        if(!vo.ContainsKey(8) || ((vo.ContainsKey(8) && vo[8].Distance >= 10)))
+                        if (!vo.ContainsKey(8) || ((vo.ContainsKey(8) && vo[8].Distance >= 10)))
                         {
                             Debug.Log("Sensor 8 good!");
 
                             Debug.Log("Passing Conditions Met!");
-                            userCarAI.IsPassing = true;
+
+                            if (Input.GetKey(KeyCode.Return))
+                            {
+                                Debug.Log("Passing Track Created!");
+                                Transform carTransform = UserCar.transform;
+                                WaypointCircuit passTrack = (WaypointCircuit)(Instantiate(passingTrack, carTransform.position + carTransform.forward * 2f, carTransform.rotation));
+                                Debug.Log("UserCar: " + carTransform.position.ToString());
+
+
+
+
+
+                            }
                         }
                     }
                 }
@@ -198,14 +242,120 @@ namespace VRAVE
 
         public void PassingInstruction_Enter()
         {
-            //hudController.model = new DefaultHUD();
+            Debug.Log("Entered: PassingInstruction");
+            //GameObject[] go = GameObject.FindGameObjectsWithTag("Path");
+            //WaypointCircuit wc = GameObject.Find("Figure8_North_3-22").GetComponent<WaypointCircuit>();
+            AIVehicleAI.Circuit = initialTrack;
+            (AIVehicle.GetComponent("Halo") as Behaviour).enabled = true;
+
+            if (Input.GetKey(KeyCode.Return))
+            {
+                ChangeState(States.Passing);
+            }
+        }
+
+        /* PASSING */
+
+        public void Passing_Enter()
+        {
+            Debug.Log("Entered: Passing");
+            AIVehicleAI.enabled = true;
+            AIVehicleAI.IsCircuit = true;
+
+
+            if (userMode)
+            {
+                (UserCar.GetComponent<CarUserControl>() as CarUserControl).enabled = true;
+                userCarAI.enabled = false;
+            }
+            else
+            {
+                (UserCar.GetComponent<CarUserControl>() as CarUserControl).enabled = false;
+                circuitProgressNum = Mathf.CeilToInt((circuitProgressNum - 3) / 4) * 4 + 3; //Rounds the waypoints up to multiples of 3+4n
+                userCarAI.switchCircuit(initialTrack, circuitProgressNum);
+                userCarAI.IsCircuit = true;
+                userCarAI.enabled = true;
+            }
+        }
+
+
+        public void Passing_Update()
+        {
+            Debug.Log("Update: Passing");
+
+            Dictionary<int, VRAVE.VRAVEObstacle> vo;
+            bool scanned = userCarSensors.Scan(out vo);
+            Debug.Log(scanned);
+            if (scanned)
+            {
+                Debug.Log("Scanning!");
+                if (vo.ContainsKey(7) && vo[7].obstacleTag.Equals("AI_Car"))
+                {
+                    Debug.Log("Sensor 7 Hit");
+                    if (vo[7].Distance <= 10f)
+                    {
+                        Debug.Log("Sensor 7 in length");
+                        if (!vo.ContainsKey(8) || ((vo.ContainsKey(8) && vo[8].Distance >= 10)))
+                        {
+                            Debug.Log("Sensor 8 good!");
+
+                            Debug.Log("Passing Conditions Met!");
+                            userCarAI.IsPassing = true; //Should be set in the first trigger of the passing path
+                            if (Input.GetKey(KeyCode.Return))
+                            {
+                                Debug.Log("Passing Track Created!");
+                                Transform carTransform = UserCar.transform;
+                                WaypointCircuit passTrack = (WaypointCircuit)(Instantiate(passingTrack, carTransform.position + carTransform.forward * 2f, carTransform.rotation));
+                                Debug.Log("UserCar: " + carTransform.position.ToString());
+                                ChangeState(States.PassingWait);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+        /* Passing Wait */
+        /* Wait while passing other vehicle as to not attempt to pass again until finished. */
+        public void PassingWait_Enter()
+        {
+            Debug.Log("Enter: PassingWait");
+            (AIVehicle.GetComponent("Halo") as Behaviour).enabled = false;  //Turn off glow as you pass.
+            userCarAI.IsPassing = true;
+            userCarAI.IsCircuit = false;
+
+            circuitProgressNum = userCarAI.ProgressNum;
+            WaypointCircuit passTemp = (WaypointCircuit)GameObject.FindGameObjectWithTag("PassingPath").GetComponent<WaypointCircuit>();
+            userCarAI.switchCircuit(passTemp, 0);
+
+            (UserCar.GetComponent<CarUserControl>() as CarUserControl).enabled = false;
+            userCarController.MaxSpeed = 50;
+            userCarAI.enabled = true;
+            
 
         }
 
-        ///* Internal scenario methods */
-        //private bool checkCarSpeed()
-        //{
-        //    return (userCarController.CurrentSpeed < intersectionStopThreshold) ? true : false;
-        //}
+        public void PassingWait_Exit()
+        {
+            Debug.Log("Exit: PassingWait");
+            userCarAI.IsPassing = false;
+           
+
+            if (userMode)
+            {
+                (UserCar.GetComponent<CarUserControl>() as CarUserControl).enabled = true;
+                userCarAI.enabled = false;
+            }
+            else
+            {
+                (UserCar.GetComponent<CarUserControl>() as CarUserControl).enabled = false;
+                circuitProgressNum = Mathf.CeilToInt((circuitProgressNum - 3) / 4) * 4 + 3; //Rounds the waypoints up to multiples of 3+4n
+                userCarAI.switchCircuit(initialTrack, circuitProgressNum);
+                userCarAI.IsCircuit = true;
+                userCarAI.enabled = true;
+            }
+
+        }
     }
 }
