cf35bb0c4bf8695b732dee174c68a7fc5240dc0b
aemery <agemery2@gmail.com>
2016-03-30 15:54:38 -0400

    WeatherScenario-Dev: work on weather scenario. committing to save work; not done yet
    created scene, scenario class

---
+++ Assets/Scripts/Scenarios/WeatherScenario/WeatherScenario.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+using MonsterLove.StateMachine;
+
+namespace VRAVE {
+
+	public class WeatherScenario : StateBehaviour {
+
+		[SerializeField] private GameObject UserCar; 
+
+		private SpawnController manufacturer; 
+		private HUDController hudController;
+		private HUDAudioController audioController;
+
+		private float FOG_DENSITY = 0.8f;
+
+		public enum States 
+		{
+			UserDriveRoute,
+			AIDriveRoute,
+			UserApproachTraffic,
+			AIApproachTraffic,
+			UserStopped,
+			AIStopped
+		}
+
+		void Awake()
+		{
+			Initialize<States> ();
+
+			UserCar.GetComponent<CarAIControl> ().enabled = false;
+			UserCar.GetComponent<CarUserControl> ().enabled = true;
+
+			manufacturer = GetComponent<SpawnController>();
+			hudController = UserCar.GetComponentInChildren<HUDController>();
+			audioController = UserCar.GetComponent<HUDAudioController>();
+
+			//hudController.model = new DefaultHUD ();
+
+			resetScenario ();
+			ChangeState(States.UserDriveRoute);
+		}
+
+		private void resetScenario() {
+			RenderSettings.fog = true; //enable fog bruh
+			RenderSettings.fogMode = FogMode.ExponentialSquared;
+			RenderSettings.fogDensity = FOG_DENSITY;
+		}
+
+		public override void TriggerCb (uint id) {
+			switch (id) {
+			//will fill out later
+			default:
+				break;
+			}
+		}
+
+		public void UserDriveRoute_Enter() {
+			//RenderSettings.fog = true;
+		}
+
+		public void AIDriveRoute_Enter() {
+			//TODO
+		}
+
+		public void UserApproachTraffic_Enter() {
+			//TODO
+		}
+
+		public void AIApproachTraffic_Enter() {
+			//TODO
+		}
+
+		public void UserStopped_Enter() {
+			//TODO
+		}
+
+		public void AIStopped_Enter() {
+			//TODO
+		}
+
+		// Extend abstract method "ChangeState(uint id)
+		//
+		// This is used for reacting to "OnTriggerEnter" events, called by WaypointTrigger scripts
+		/*public override void ChangeState (uint id)
+		{
+			switch (id) 
+			{
+			case 0: 
+				break;
+			}
+		}*/
+	}
+}
+		
