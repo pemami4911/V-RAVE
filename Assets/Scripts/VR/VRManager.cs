561d9c2c88eba9009e31b2ffc47dc5fddb59d62f
aemery <agemery2@gmail.com>
2016-03-30 15:48:18 -0400

    Oculus-Dev: add VRManager to poll for escape key press & quit game

---
+++ Assets/Scripts/VR/VRManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VRManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("escape")) {
+			Debug.Log ("excape key pressed");
+			Application.Quit(); // Quits the game
+		}
+	}
+}
