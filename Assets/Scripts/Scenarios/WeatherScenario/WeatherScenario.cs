cea74bb43969d03640e1d22f6c868783972b5c37
aemery <agemery2@gmail.com>
2016-04-04 00:37:44 -0400

    WeatherScenario-Dev: work on scenario
    added states, added dark ai car prefab

--- Assets/Scripts/Scenarios/WeatherScenario/WeatherScenario.cs
+++ Assets/Scripts/Scenarios/WeatherScenario/WeatherScenario.cs
@@ -13,7 +13,7 @@ namespace VRAVE {
 		private HUDController hudController;
 		private HUDAudioController audioController;
 
-		private float FOG_DENSITY = 0.1f;
+		private float INITIAL_FOG_DENSITY = 0.1f;
 
 		public enum States 
 		{
@@ -21,6 +21,8 @@ namespace VRAVE {
 			AIDriveRoute,
 			UserApproachTraffic,
 			AIApproachTraffic,
+			UserWarnCollision,
+			AIWarnCollision,
 			UserStopped,
 			AIStopped
 		}
@@ -47,7 +49,7 @@ namespace VRAVE {
 		private void resetScenario() {
 			RenderSettings.fog = true; //enable fog bruh
 			RenderSettings.fogMode = FogMode.Exponential;
-			RenderSettings.fogDensity = FOG_DENSITY;
+			RenderSettings.fogDensity = INITIAL_FOG_DENSITY;
 			hudController.model = new WeatherHUDModel ();
 		}
 
@@ -61,6 +63,23 @@ namespace VRAVE {
 					ChangeState(States.AIApproachTraffic);
 				}
 				break;
+			case 1: //increase fog
+				if (GetState ().Equals (States.UserApproachTraffic) || GetState ().Equals (States.AIApproachTraffic)) {
+					RenderSettings.fogDensity = 0.8f;
+				}
+				break;
+			case 2:
+				if (GetState ().Equals (States.UserApproachTraffic)) {
+					ChangeState (States.UserWarnCollision);
+				} else if (GetState().Equals (States.AIApproachTraffic)) {
+					ChangeState (States.AIWarnCollision);
+				}
+				break;
+			case 3:
+				if (GetState ().Equals (States.UserDriveRoute) || GetState ().Equals (States.AIDriveRoute)) {
+					hudController.model.centerText = "Turn left";
+				}
+				break;
 			default:
 				break;
 			}
@@ -68,7 +87,7 @@ namespace VRAVE {
 
 		public void UserDriveRoute_Enter() {
 			spawnController.enterScenario ();
-
+			hudController.model.centerText = "Proceed to intersection";
 		}
 
 		public void AIDriveRoute_Enter() {
@@ -77,11 +96,21 @@ namespace VRAVE {
 
 		public void UserApproachTraffic_Enter() {
 			Debug.Log("Entered State UserApproachTraffic");
-			hudController.model.isLeftImageEnabled = true;
+			//increase fog in this state
+			RenderSettings.fogDensity = 0.3f;
 		}
 
 		public void AIApproachTraffic_Enter() {
 			//TODO
+		}
+
+		public void UserWarnCollision_Enter() {
+			hudController.model.isLeftImageEnabled = true;
+		}
+
+		public void AIWarnCollision_Enter() {
+			//TODO
+			hudController.model.isLeftImageEnabled = true;
 		}
 
 		public void UserStopped_Enter() {
